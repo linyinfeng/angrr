658496fef18cbc8dbc05ae16a51c22e088f71d83
Alan Pearce <alan@alanpearce.eu>
2025-07-01 17:23:47 +0200

    enable formatter on darwin

--- flake.nix
+++ flake.nix
@@ -27,6 +27,8 @@
         systems = [
           "x86_64-linux"
           "aarch64-linux"
+          "x86_64-darwin"
+          "aarch64-darwin"
         ];
         imports = [
           inputs.flake-parts.flakeModules.easyOverlay
